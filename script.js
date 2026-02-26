0c4060a1e7f9883f458bc881c113653b52858909
fasilafaaz06-eng <fasilafaaz06@gmail.com>
2026-02-26 23:00:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const calculate = () => {
   let amount = p - simpleInterest;
 
   let result = document.getElementById("result");
+  script.js:3 Uncaught TypeError: Cannot read properties of null (reading 'value')
+    at calculate (script.js:3:47)
+    at HTMLButtonElement.onclick ((index):31:58)
+
 
   result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
